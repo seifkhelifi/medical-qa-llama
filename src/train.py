2bec85130c69722ecac9ffec0a0c4bf13a1434ff
Seif Khelifi <khelifiseif1@gmail.com>
2025-10-17 20:22:51 +0100

    original

--- src/train.py
+++ src/train.py
@@ -105,7 +105,7 @@ if is_main_process():
 # ==========================================================
 model = FastLanguageModel.get_peft_model(
     model,
-    r=16,
+    r=16,  # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
     target_modules=[
         "q_proj",
         "k_proj",
@@ -116,12 +116,13 @@ model = FastLanguageModel.get_peft_model(
         "down_proj",
     ],
     lora_alpha=16,
-    lora_dropout=0,
-    bias="none",
-    use_gradient_checkpointing="unsloth",
+    lora_dropout=0,  # Supports any, but = 0 is optimized
+    bias="none",  # Supports any, but = "none" is optimized
+    # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+    use_gradient_checkpointing="unsloth",  # True or "unsloth" for very long context
     random_state=3407,
-    use_rslora=False,
-    loftq_config=None,
+    use_rslora=False,  # We support rank stabilized LoRA
+    loftq_config=None,  # And LoftQ
 )
 
 
